6837d9582860af1874462c85e5a93c6f3be028eb
Samiul Hossain <56602986+Samiul-Hossain@users.noreply.github.com>
2022-08-04 21:56:57 +0600

    Time: 87 ms (67.40%), Space: 45 MB (50.33%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,74 +1,55 @@
-class TrieNode{
-    public:
-    bool isWord;
-    TrieNode* children[26];
-    TrieNode(){
-        for(int i=0; i<26; i++) children[i] = NULL;
-        isWord = false;
-    }
-};
-class Trie {
+class TrieNode {
 public:
-    Trie() {
-        root = new TrieNode();
-    }
-    
-    void insert(string word) {
-        TrieNode* curr = root;;
-        int k = 0;
-        
-        for(int i=0; word[i]; i++)
-        {    
-            k = word[i] - 'a';
-            
-            if(!curr->children[k])
-            {
-                curr->children[k] = new TrieNode();
-            }
-            curr = curr->children[k];
-        }
-        
-        curr->isWord = true;
-    }
-    
-    bool search(string word) {
-        TrieNode* curr = root;
-        int k = 0;
-        
-        for(int i=0; word[i]; i++){
-            k = word[i] - 'a';
-            if(!curr->children[k]){
-                return false;
-            }
-            curr = curr->children[k];
-        }
-        return curr->isWord;
-    }
-    
-    bool startsWith(string prefix) {
-        int word_len = prefix.length();
-        int k = 0;
-        TrieNode *cur = root;
-        
-        for (int i = 0; i < word_len; i++)
-        {
-            k = prefix[i] - 'a';
-            cur = cur->children[k];
-            
-            if (cur == NULL)
-                return false;
-        }
-        
-        return true;
-    }
-private:
-    TrieNode* root;
+	bool isWord;
+	TrieNode* children[26];
+	TrieNode() {
+		for (int i = 0; i < 26; i++) children[i] = NULL;
+		isWord = false;
+	}
 };
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
+class Trie {
+public:
+	Trie() {
+		root = new TrieNode();
+	}
+
+	void insert(string word) {
+		TrieNode* curr = root;
+		int k = 0;
+		for (int i = 0; word[i]; i++) {
+			k = word[i] - 'a';
+			if (!curr->children[k]) {
+				curr->children[k] = new TrieNode();
+			}
+			curr = curr->children[k];
+		}
+        curr->isWord = true;
+	}
+
+	bool search(string word) {
+		TrieNode* curr = root;
+		int k = 0;
+		for (int i = 0; word[i]; i++) {
+			k = word[i] - 'a';
+			if (!curr->children[k])
+				return false;
+			curr = curr->children[k];
+		}
+		return curr->isWord;
+	}
+
+	bool startsWith(string prefix) {
+		TrieNode* curr = root;
+		int k = 0;
+		for (int i = 0; prefix[i]; i++) {
+			k = prefix[i] - 'a';
+			curr = curr->children[k];
+			if (!curr)
+				return false;
+		}
+		return true;
+	}
+private:
+	TrieNode* root;
+};
